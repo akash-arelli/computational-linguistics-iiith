cd1d590e54410dbf3cf851945dd7cc53d8f5924b
akash-arelli <akasharelli@gmail.com>
2020-07-10 14:16:23 +0530

    goal 2

---
+++ RDIP/Codes/home.js
@@ -0,0 +1,18 @@
+function hello(){
+ 
+
+var sts=[{},{a:"John ate an apple before afternoon",b:"before afternoon John ate an apple",c:"John before afternoon ate an apple"},
+{a:"some students like to study in the night",b:"at night some students like to study"},
+{a:"John and Mary went to church",b:"Mary and John went to church"},
+{a:"John went to church after eating",b:"after eating John went to church",c:"John after eating went to church"},
+{a:"did he go to market	",b:"he did go to market"},
+{a:"the woman who called my sister sells cosmetics",b:"the woman who sells cosmetics called my sister",c:"my sister who sells cosmetics called the woman",d:"my sister who called the woman sells cosmetics"},
+{a:"John goes to the library and studies",b:"John studies and goes to the library"},
+{a:"John ate an apple so did she",b:"she ate an apple so did John"},
+{a:"the teacher returned the book after she noticed the error",b:"the teacher noticed the error after she returned the book",c:"after the teacher returned the book she noticed the error",d:"after the teacher noticed the error she returned the book",e:"she returned the book after the teacher noticed the error",f:"she noticed the error after the teacher returned the book",g:"after she returned the book the teacher noticed the error",h:"after she noticed the error the teacher returned the book"},
+{a:"I told her that I bought a book yesterday",b:"I told her yesterday that I bought a book",c:"yesterday I told her that I bought a book",d:"I bought a book that I told her yesterday",e:"I bought a book yesterday that I told her	",f:"yesterday I bought a book that I told her"}];
+
+
+
+
+}
