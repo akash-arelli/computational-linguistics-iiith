f9d92b184bcd2e319fb66b13542c796da63407de
akash-arelli <akasharelli@gmail.com>
2020-07-10 17:24:07 +0530

    goal 3

--- RDIP/Codes/home.js
+++ RDIP/Codes/home.js
@@ -1,7 +1,6 @@
-function hello(){
- 
 
-var sts=[{},{a:"John ate an apple before afternoon",b:"before afternoon John ate an apple",c:"John before afternoon ate an apple"},
+
+var english=[{},{a:"John ate an apple before afternoon",b:"before afternoon John ate an apple",c:"John before afternoon ate an apple"},
 {a:"some students like to study in the night",b:"at night some students like to study"},
 {a:"John and Mary went to church",b:"Mary and John went to church"},
 {a:"John went to church after eating",b:"after eating John went to church",c:"John after eating went to church"},
@@ -14,5 +13,31 @@ var sts=[{},{a:"John ate an apple before afternoon",b:"before afternoon John ate
 
 
 
+var hindi=[{},{a:"राम और श्याम बाजार गयें",b:"राम और श्याम गयें बाजार",c:"बाजार गयें राम और श्याम",d:"गयें बाजार राम और श्याम"},
+{a:"राम सोया और श्याम भी",b:"श्याम सोया और राम भी",c:"सोया श्याम और राम भी",d:"सोया राम और श्याम भी"},
+{a:"मैंने उसे बताया कि राम सो रहा है",b:"मैंने उसे बताया कि सो रहा है राम",c:"उसे मैंने बताया कि राम सो रहा है",d:"उसे मैंने बताया कि सो रहा है राम",e:"मैंने बताया उसे कि राम सो रहा है",f:"मैंने बताया उसे कि सो रहा है राम",g:"उसे बताया मैंने कि राम सो रहा है",h:"उसे बताया मैंने कि सो रहा है राम",i:"बताया मैंने उसे कि राम सो रहा है",j:"बताया मैंने उसे कि सो रहा है राम",k:"बताया उसे मैंने कि राम सो रहा है",l:"बताया उसे मैंने कि सो रहा है राम"},
+{a:"बिल्लियों को मारकर कुत्ता सो गया	",b:"मारकर बिल्लियों को कुत्ता सो गया	",c:"बिल्लियों को मारकर सो गया कुत्ता	",d:"मारकर बिल्लियों को सो गया कुत्ता	",e:"कुत्ता सो गया बिल्लियों को मारकर	",f:"कुत्ता सो गया मारकर बिल्लियों को	",g:"सो गया कुत्ता बिल्लियों को मारकर",h:"सो गया कुत्ता मारकर बिल्लियों को"},
+{a:"एक लाल किताब वहाँ है",b:"एक लाल किताब है वहाँ",c:"वहाँ है एक लाल किताब",d:"है वहाँ एक लाल किताब"},
+
+{a:"एक बड़ी सी किताब वहाँ है	",b:"एक बड़ी सी किताब है वहाँ",c:"बड़ी सी एक किताब वहाँ है",d:"बड़ी सी एक किताब है वहाँ",e:"वहाँ है एक बड़ी सी किताब",f:"वहाँ है बड़ी सी एक किताब",g:" है वहाँ एक बड़ी सी किताब",h:"है वहाँ बड़ी सी एक किताब"}];
+
+
+ document.getElementById("pri").innerHTML="new";
+
+var script = document.createElement('script');
+script.src = 'jquery-3.5.1.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
+function mn(){
+
+     $(document).ready(function(){
+  $("#language").change(function(){
+    $("#p1").show();
+    $("#p2").show();
+  });
+});
 
 }
+
+
