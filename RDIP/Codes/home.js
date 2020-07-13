5d212553fc7198a84203d566a9a232c67f88565c
akash-arelli <akasharelli@gmail.com>
2020-07-13 20:29:29 +0530

    goal 8

--- RDIP/Codes/home.js
+++ RDIP/Codes/home.js
@@ -1,25 +1,25 @@
 
 
-var english=[{},{a:"John ate an apple before afternoon",b:"before afternoon John ate an apple",c:"John before afternoon ate an apple"},
-    {a:"some students like to study in the night",b:"at night some students like to study"},
-    {a:"John and Mary went to church",b:"Mary and John went to church"},
-    {a:"John went to church after eating",b:"after eating John went to church",c:"John after eating went to church"},
-    {a:"did he go to market	",b:"he did go to market"},
-    {a:"the woman who called my sister sells cosmetics",b:"the woman who sells cosmetics called my sister",c:"my sister who sells cosmetics called the woman",d:"my sister who called the woman sells cosmetics"},
-    {a:"John goes to the library and studies",b:"John studies and goes to the library"},
-    {a:"John ate an apple so did she",b:"she ate an apple so did John"},
-    {a:"the teacher returned the book after she noticed the error",b:"the teacher noticed the error after she returned the book",c:"after the teacher returned the book she noticed the error",d:"after the teacher noticed the error she returned the book",e:"she returned the book after the teacher noticed the error",f:"she noticed the error after the teacher returned the book",g:"after she returned the book the teacher noticed the error",h:"after she noticed the error the teacher returned the book"},
-    {a:"I told her that I bought a book yesterday",b:"I told her yesterday that I bought a book",c:"yesterday I told her that I bought a book",d:"I bought a book that I told her yesterday",e:"I bought a book yesterday that I told her	",f:"yesterday I bought a book that I told her"}];
+var english=[[],["John ate an apple before afternoon","before afternoon John ate an apple","John before afternoon ate an apple"],
+    ["some students like to study in the night","at night some students like to study"],
+    ["John and Mary went to church","Mary and John went to church"],
+    ["John went to church after eating","after eating John went to church","John after eating went to church"],
+    ["did he go to market	","he did go to market"],
+    ["the woman who called my sister sells cosmetics","the woman who sells cosmetics called my sister","my sister who sells cosmetics called the woman","my sister who called the woman sells cosmetics"],
+    ["John goes to the library and studies","John studies and goes to the library"],
+    ["John ate an apple so did she","she ate an apple so did John"],
+    ["the teacher returned the book after she noticed the error","the teacher noticed the error after she returned the book","after the teacher returned the book she noticed the error","after the teacher noticed the error she returned the book","she returned the book after the teacher noticed the error","she noticed the error after the teacher returned the book","after she returned the book the teacher noticed the error","after she noticed the error the teacher returned the book"],
+    ["I told her that I bought a book yesterday","I told her yesterday that I bought a book","yesterday I told her that I bought a book","I bought a book that I told her yesterday","I bought a book yesterday that I told her	","yesterday I bought a book that I told her"]];
 
 
 
-var hindi=[{},{a:"राम और श्याम बाजार गयें",b:"राम और श्याम गयें बाजार",c:"बाजार गयें राम और श्याम",d:"गयें बाजार राम और श्याम"},
-    {a:"राम सोया और श्याम भी",b:"श्याम सोया और राम भी",c:"सोया श्याम और राम भी",d:"सोया राम और श्याम भी"},
-    {a:"मैंने उसे बताया कि राम सो रहा है",b:"मैंने उसे बताया कि सो रहा है राम",c:"उसे मैंने बताया कि राम सो रहा है",d:"उसे मैंने बताया कि सो रहा है राम",e:"मैंने बताया उसे कि राम सो रहा है",f:"मैंने बताया उसे कि सो रहा है राम",g:"उसे बताया मैंने कि राम सो रहा है",h:"उसे बताया मैंने कि सो रहा है राम",i:"बताया मैंने उसे कि राम सो रहा है",j:"बताया मैंने उसे कि सो रहा है राम",k:"बताया उसे मैंने कि राम सो रहा है",l:"बताया उसे मैंने कि सो रहा है राम"},
-    {a:"बिल्लियों को मारकर कुत्ता सो गया	",b:"मारकर बिल्लियों को कुत्ता सो गया	",c:"बिल्लियों को मारकर सो गया कुत्ता	",d:"मारकर बिल्लियों को सो गया कुत्ता	",e:"कुत्ता सो गया बिल्लियों को मारकर	",f:"कुत्ता सो गया मारकर बिल्लियों को	",g:"सो गया कुत्ता बिल्लियों को मारकर",h:"सो गया कुत्ता मारकर बिल्लियों को"},
-    {a:"एक लाल किताब वहाँ है",b:"एक लाल किताब है वहाँ",c:"वहाँ है एक लाल किताब",d:"है वहाँ एक लाल किताब"},
+var hindi=[[],["राम और श्याम बाजार गयें","राम और श्याम गयें बाजार","बाजार गयें राम और श्याम","गयें बाजार राम और श्याम"],
+    ["राम सोया और श्याम भी","श्याम सोया और राम भी","सोया श्याम और राम भी","सोया राम और श्याम भी"],
+    ["मैंने उसे बताया कि राम सो रहा है","मैंने उसे बताया कि सो रहा है राम","उसे मैंने बताया कि राम सो रहा है","उसे मैंने बताया कि सो रहा है राम","मैंने बताया उसे कि राम सो रहा है","मैंने बताया उसे कि सो रहा है राम","उसे बताया मैंने कि राम सो रहा है","उसे बताया मैंने कि सो रहा है राम","बताया मैंने उसे कि राम सो रहा है","बताया मैंने उसे कि सो रहा है राम","बताया उसे मैंने कि राम सो रहा है","बताया उसे मैंने कि सो रहा है राम"],
+    ["बिल्लियों को मारकर कुत्ता सो गया	","मारकर बिल्लियों को कुत्ता सो गया	","बिल्लियों को मारकर सो गया कुत्ता	","मारकर बिल्लियों को सो गया कुत्ता	","कुत्ता सो गया बिल्लियों को मारकर	","कुत्ता सो गया मारकर बिल्लियों को	","सो गया कुत्ता बिल्लियों को मारकर","सो गया कुत्ता मारकर बिल्लियों को"],
+    ["एक लाल किताब वहाँ है","एक लाल किताब है वहाँ","वहाँ है एक लाल किताब","है वहाँ एक लाल किताब"],
 
-    {a:"एक बड़ी सी किताब वहाँ है	",b:"एक बड़ी सी किताब है वहाँ",c:"बड़ी सी एक किताब वहाँ है",d:"बड़ी सी एक किताब है वहाँ",e:"वहाँ है एक बड़ी सी किताब",f:"वहाँ है बड़ी सी एक किताब",g:" है वहाँ एक बड़ी सी किताब",h:"है वहाँ बड़ी सी एक किताब"}];
+    ["एक बड़ी सी किताब वहाँ है	","एक बड़ी सी किताब है वहाँ","बड़ी सी एक किताब वहाँ है","बड़ी सी एक किताब है वहाँ","वहाँ है एक बड़ी सी किताब","वहाँ है बड़ी सी एक किताब"," है वहाँ एक बड़ी सी किताब","है वहाँ बड़ी सी एक किताब"]];
 
 
 
@@ -42,6 +42,9 @@ function mn(){
 }
 var c=document.getElementById("new").childNodes;
 var k=0;
+var no=0;
+var element1=document.getElementById("new2");
+
 function randomall(){
 
     var obj=document.getElementById("language").value;
@@ -49,9 +52,9 @@ function randomall(){
     if(obj=="English") {
 
 
-        var no = Math.floor(Math.random() * 10) + 1;
+        no = Math.floor(Math.random() * 10) + 1;
 
-        var str = english[no].a;
+        var str = english[no][1];
         var total = str.split(" ").length;
 
         var no1 = Math.floor(Math.random() * total) + 1;
@@ -88,7 +91,7 @@ function randomall(){
                     var spa=" ";
                     var textnode = document.createTextNode(k+" ");
                     node.appendChild(textnode);
-                    var element1=document.getElementById("new2");
+                    element1=document.getElementById("new2");
                     element1.appendChild(node);
                     para++;
 
@@ -116,9 +119,9 @@ function randomall(){
     {
 
 
-        var no=Math.floor(Math.random() * 7) + 1;
+        no=Math.floor(Math.random() * 7) + 1;
 
-        var str=hindi[no].a;
+        var str=hindi[no][1];
         var total = str.split(" ").length;
 
         var no1=Math.floor(Math.random() * total) + 1;
@@ -191,6 +194,7 @@ function vanish(){
     $("#new2").hide();
     $("#new3").hide();
     $("#new5").hide();
+    $("#new6").hide();
     for(var i=0;i<c.length;i++)
         $(c[i]).show();
     var para=0;
@@ -226,5 +230,30 @@ function vanish(){
         })(i);
     }
 
+
+}
+
+function check() {
+    $("#new6").show();
+    var str = "hello";
+    var nk=english[no];
+    var len=nk.length;
+    var mic = $("#new2").closest("p").text();
+    var int=0;
+    for(var i=0;i<len;i++)
+    {
+        if (mic.trim()==english[no][i].trim()) {
+            str = "Right answer!!!";
+            document.getElementById("new6").style.color="green";
+            int++;
+        }
+
+
+    }
+    if(int ==0){
+        str = "Wrong answer!!!";
+        document.getElementById("new6").style.color="red";
+    }
+    document.getElementById("new6").innerHTML =str ;
 
 }
